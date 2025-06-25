4c4dcc7026e90bfe7854662284e8b9c7b8160fb7
kusuru-001 <ksaigowtham@gmail.com>
2025-06-25 21:26:59 +0530

    Final

--- Program2.js
+++ Program2.js
@@ -1,11 +1,11 @@
-function generateSeries(input){
-let i = 1;
-let series = [1];
-while(input != series.length){
-    i=i+2;
-    series.push(i);
-}
-return series.join();
+function generateSeries(input) {
+    let i = 1;
+    let series = [1];
+    while (input != series.length) {
+        i = i + 2;
+        series.push(i);
+    }
+    return series.join();
 }
 
 console.log(generateSeries(10))
