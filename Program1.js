6e59eb726d0206a21db40a2fd27fb32ace324be7
Gowtham <40486258+kusuru-001@users.noreply.github.com>
2025-06-25 21:17:50 +0530

    Add files via upload

---
+++ Program1.js
@@ -0,0 +1,13 @@
+function calculator(input1,input2,operation){
+    if(operation=='Addition'){
+        return input1+ input2;
+    }else if(operation=='Multiplication'){
+        return input1*input12
+    }else if(operation=='Division'){
+        return input1/input2;
+    }else{
+        console.log('Error')
+    }
+}
+
+console.log(calculator(2,3,'Division'))
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # TANDEMLOOP
 Tandemloop solutions
+# PreReq
+The solutions are solved using JavaScript
+
+Install nodejs in your machine
+
+# RUN Programs
+open command prompt/terminal and run below command
+```bash
+node [program-name].js
+ex:- node Program1.js
